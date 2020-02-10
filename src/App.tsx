d2af150c38c20ec6e8625758836a477275213710
Ner Karso <nerpkarso@gmail.com>
2020-02-10 20:00:40 +0530

    Add section

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@ const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <section className="section">
+          <header className="section__header">
+            <h2 className="section__title">Subjects</h2>
+          </header>
+          <div className="section__body">
+          </div>
+        </section>
+      </main>
     </>
   );
 };
