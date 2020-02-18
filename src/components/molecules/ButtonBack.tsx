ca10654bff2fb2b2af5774dc528bcb1850f9ae41
Ner Karso <nerpkarso@gmail.com>
2020-02-18 21:13:20 +0530

    Add support for click handler to back button

--- src/components/molecules/ButtonBack.tsx
+++ src/components/molecules/ButtonBack.tsx
@@ -4,17 +4,17 @@ import { useHistory } from 'react-router-dom';
 
 import Button from '../atoms/Button';
 
-interface Props {}
+interface Props {
+  onClick?: () => void;
+}
 
-const ButtonBack: React.FC<Props> = () => {
+const ButtonBack: React.FC<Props> = ({ onClick }) => {
   const history = useHistory();
 
+  const clickHandler = () => (onClick ? onClick() : history.goBack());
+
   return (
-    <Button
-      className="button--leading"
-      onClick={() => history.goBack()}
-      title="Back"
-    >
+    <Button className="button--leading" onClick={clickHandler} title="Go back">
       <ArrowLeft color="var(--text)" size={24} />
     </Button>
   );
