17b6afdf111559b3e02b048d0b144b859a099fdd
Ner Karso <nerpkarso@gmail.com>
2020-11-27 20:36:42 +0530

    Add spinner when authenticating

--- middlewares/useMockLogin.js
+++ middlewares/useMockLogin.js
@@ -3,9 +3,13 @@ const mockLogin = (handler) => (req, res) => {
 
   const { body } = req;
   if (body.username === '2020' && body.password === '2020') {
-    return res.json({ token: 'foo' });
+    return res.json({
+      auth: true,
+      userToken:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjIwMjAiLCJwYXNzd29yZCI6IjIwMjAiLCJpYXQiOjE2MDY0ODk1MTR9.sZ_Ci0uEYNd5a8RntJwJaHkbyZAEvM0kD28zYz4LIc0',
+    });
   } else {
-    return res.json({ error: true, message: 'Your username or password is incorrect' });
+    return res.json({ auth: false, message: 'Your username or password is incorrect' });
   }
 };
 
--- pages/login.js
+++ pages/login.js
@@ -40,6 +40,7 @@ export default function Login() {
     <>
       <Head>
         <title>Log in | PTU Exam Portal</title>
+        <link rel="icon" href="/icon.png" />
       </Head>
       <main className="grid h-full place-items-center">
         <div className="w-full max-w-sm p-6 rounded sm:border border-base-300 dark:border-base-700">
@@ -69,8 +70,34 @@ export default function Login() {
                 required
               />
             </div>
-            <button type="submit" className="w-full mt-6 btn btn--primary" disabled={loading}>
-              {loading ? 'Authenticating...' : 'Log in'}
+            <button
+              type="submit"
+              className={'w-full mt-4 btn btn--primary' + (loading ? ' cursor-not-allowed' : '')}
+              disabled={loading}>
+              {loading ? (
+                <span className="flex items-center gap-3">
+                  <svg
+                    className="w-5 h-5 animate-spin"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24">
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+                  Authenticating...
+                </span>
+              ) : (
+                'Log in'
+              )}
             </button>
           </form>
         </div>
