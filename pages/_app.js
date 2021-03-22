1cccaad68ffb7f8ab1c334103744574ba157bcd5
Ner Karso <nerpkarso@gmail.com>
2021-03-22 21:48:08 +0530

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import { AuthProvider } from '@/hooks/AuthContext';
 import '@/styles/index.css';
+import { pageview } from '@/utils/gtag';
 import { ThemeProvider } from 'next-themes';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { SWRConfig } from 'swr';
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => pageview(url);
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);
+
   return (
     <ThemeProvider attribute="class">
       <AuthProvider>
