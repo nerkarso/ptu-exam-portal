e9170ebc7efe6f392c4bbf2b119cd388e4474333
Ner Karso <nerpkarso@gmail.com>
2021-04-23 05:34:41 +0530

    Update switch focus style

--- elements/Switch.js
+++ elements/Switch.js
@@ -1,12 +1,12 @@
 export default function Switch({ id, checked, onChange }) {
   return (
-    <div className="relative inline-block w-10">
+    <div className="relative inline-block w-10 transition duration-300 rounded-full focus-within:ring-2 focus-within:ring-primary-400">
       <input
         type="checkbox"
         id={id}
         checked={checked}
         onChange={onChange}
-        className="absolute w-6 h-6 transition duration-300 transform bg-white border rounded-full shadow appearance-none cursor-pointer dark:bg-invert-500 dark:border-invert-700 switch-control border-base-200 focus:outline-none focus:ring-2 ring-primary-400"
+        className="absolute w-6 h-6 transition duration-300 transform bg-white border rounded-full shadow appearance-none cursor-pointer dark:bg-invert-500 dark:border-invert-700 switch-control border-base-200 focus:outline-none"
       />
       <label
         htmlFor={id}
