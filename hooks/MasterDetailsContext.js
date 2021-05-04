8e3c3dd0270e288477b330273b3debf72a90958c
Ner Karso <nerpkarso@gmail.com>
2021-05-04 08:02:31 +0530

    Add ability to close preview on back click

--- hooks/MasterDetailsContext.js
+++ hooks/MasterDetailsContext.js
@@ -1,11 +1,30 @@
-import { createContext, useState } from 'react';
+import { useRouter } from 'next/router';
+import { createContext, useEffect, useState } from 'react';
 
 export const MasterDetailsContext = createContext(null);
 
 export function MasterDetailsProvider({ children }) {
+  const router = useRouter();
   const [details, setDetailState] = useState(null);
-  const setDetails = (value) => setDetailState(value);
-  const resetDetails = () => setDetails(null);
+  const setDetails = (value) => {
+    if (value) {
+      router.push('#preview-details');
+    }
+    setDetailState(value);
+  };
+  const resetDetails = () => router.back();
+
+  useEffect(() => {
+    function handleHashChange() {
+      if (window.location.hash === '') {
+        setDetails(null);
+      }
+    }
+    window.addEventListener('hashchange', handleHashChange);
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
+  }, []);
 
   return (
     <MasterDetailsContext.Provider value={{ details, setDetails, resetDetails }}>
