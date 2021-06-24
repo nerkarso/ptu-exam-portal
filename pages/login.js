087cb54181afbcc1e9980aaf7e14fdc0466a4f4d
Ner Karso <nerpkarso@gmail.com>
2021-06-24 18:38:16 +0530

    Update login page

--- pages/login.js
+++ pages/login.js
@@ -21,9 +21,15 @@ export default function Login() {
     }
   }, [isLoggedIn]);
 
+  useEffect(() => {
+    // Automatically focus on the username field
+    const usernameElem = document.querySelector('#username');
+    if (usernameElem) usernameElem.focus();
+  }, []);
+
   return (
     <div className="flex flex-col h-full">
-      <main className="grid flex-1 h-full place-items-center">
+      <main className="grid flex-grow place-items-center">
         <form className="w-full max-w-sm px-6 py-8 space-y-5" onSubmit={handleSubmit(onSubmit)}>
           <Logo />
           {error && (
@@ -46,8 +52,18 @@ export default function Login() {
           </Button>
         </form>
       </main>
-      <footer className="px-4 py-6 text-sm text-center text-base-500 dark:text-invert-500">
-        <p>&copy; 2020-{new Date().getFullYear()} Ngineer Lab. Not affiliated with IKGPTU.</p>
+      <footer className="flex-shrink-0 px-4 py-6 text-sm text-center text-base-500 dark:text-invert-500">
+        <p>
+          &copy; 2020-{new Date().getFullYear()}
+          {' Made by '}
+          <a
+            href="https://nerkarso.github.io"
+            className="font-semibold transition duration-300 text-primary-600 dark:text-primary-500 hover:text-primary-500 dark:hover:text-primary-400 focus:outline-none focus:bg-primary-500 focus:bg-opacity-20"
+            target="_blank"
+            rel="noopener noreferrer">
+            Ner Karso
+          </a>
+        </p>
       </footer>
     </div>
   );
@@ -55,9 +71,10 @@ export default function Login() {
 
 function Logo() {
   return (
-    <div className="space-y-3">
+    <div className="space-y-3 text-center">
       <img src="/logo.svg" alt="Logo" className="w-16 h-16 mx-auto" />
-      <h1 className="text-2xl font-bold text-center">Log in to continue</h1>
+      <h1 className="text-2xl font-bold">PTU Exam Portal</h1>
+      <p className="text-base-500 dark:text-invert-500">Please verify yourself to continue</p>
     </div>
   );
 }
